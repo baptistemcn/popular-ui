818ecd61039d53917349ba7f1ea51c0afef51539
baptistemcn <baptistemcn@icloud.com>
2023-05-14 19:20:02 +0200

    feat: router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import Popular from "./components/Popular";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="light">
-      <div className="container">
-        <Popular />
+    <Router>
+      <div className="light">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Popular />} />
+            <Route path="*" element={<Popular />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
