b051d7162388ab6a2b98eae4eeb27bbd1a11df03
baptistemcn <baptistemcn@icloud.com>
2023-05-14 18:05:23 +0200

    refactor: hover component

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,44 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import * as React from "react";
+import Hover from "./Hover";
 
 const container = {
   position: "relative",
   display: "flex",
 };
 
-export default class Tooltip extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hovering: false,
-    };
-
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
-
-  mouseOver() {
-    this.setState({ hovering: true });
-  }
-
-  mouseOut() {
-    this.setState({ hovering: false });
-  }
-
-  render() {
-    const { hovering } = this.state;
-    const { children, element } = this.props;
-
-    return (
-      <div
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-        style={container}
-      >
-        {hovering === true && element}
-        {children}
-      </div>
-    );
-  }
+export default function Tooltip({ children, element }) {
+  return (
+    <Hover>
+      {(hovering) => {
+        return (
+          <div style={container}>
+            {hovering === true && element}
+            {children}
+          </div>
+        );
+      }}
+    </Hover>
+  );
 }
