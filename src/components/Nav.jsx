3ab03b25e8f16c086316c7359abd1298fa890e4f
baptistemcn <baptistemcn@icloud.com>
2023-06-11 20:14:19 +0200

    refactor: theme consumer

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,37 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { sunIcon, moonIcon } from "./icons";
-import { ThemeConsumer } from "../context/theme";
+import ThemeContext from "../context/theme";
 
 // eslint-disable-next-line react/prop-types
 export default function Nav({ toggleTheme }) {
+  const theme = useContext(ThemeContext);
+
   return (
-    <ThemeConsumer>
-      {(theme) => (
-        <nav className="split">
+    <nav className="split">
+      <NavLink
+        to="/"
+        className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
+      >
+        Popular UI
+      </NavLink>
+      <ul className="row">
+        <li>
           <NavLink
             to="/"
             className={({ isActive }) =>
-              "nav-link" + (isActive ? " active" : "")
+              "nav-link " + (isActive ? " active" : "")
             }
           >
-            Popular UI
+            Popular
           </NavLink>
-          <ul className="row">
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  "nav-link " + (isActive ? " active" : "")
-                }
-              >
-                Popular
-              </NavLink>
-            </li>
-            <li>
-              <button className="btn secondary icon" onClick={toggleTheme}>
-                {theme === "light" ? moonIcon : sunIcon}
-              </button>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </ThemeConsumer>
+        </li>
+        <li>
+          <button className="btn secondary icon" onClick={toggleTheme}>
+            {theme === "light" ? moonIcon : sunIcon}
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 }
